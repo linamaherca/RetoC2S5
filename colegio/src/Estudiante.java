e43f7f51f74bedb1341c607542af2a786c03f698
linam <linam@LAPTOP-A1MAQO2K>
2020-11-30 23:24:58 -0500

    Iniciando el reto 5

---
+++ colegio/src/Estudiante.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author linam
+ */
+public class Estudiante {
+    private int id;
+    private String nombre;
+    private String celular;
+    private int edad;
+}
---
+++ colegio/src/Profesor.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author linam
+ */
+public class Profesor {
+    private int id;
+    private String nombre;
+    private String correo;
+    private int edad;
+    
+    
+}
